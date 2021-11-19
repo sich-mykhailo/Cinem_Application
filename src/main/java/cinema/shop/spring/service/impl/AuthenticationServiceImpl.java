5b78fad4e764cb025819fecdca4067d3a1488ed9
misha8096 <sichmisha@gmail.com>
2021-11-19 12:49:31 +0200

    Initial commit

--- src/main/java/cinema/shop/spring/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/cinema/shop/spring/service/impl/AuthenticationServiceImpl.java
@@ -3,8 +3,10 @@ package cinema.shop.spring.service.impl;
 import cinema.shop.spring.model.Role;
 import cinema.shop.spring.model.User;
 import cinema.shop.spring.service.AuthenticationService;
+import cinema.shop.spring.service.RoleService;
 import cinema.shop.spring.service.ShoppingCartService;
 import cinema.shop.spring.service.UserService;
+import java.util.HashSet;
 import java.util.Set;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,14 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
@@ -24,7 +29,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
-        user.setRoles(Set.of(new Role("USER")));
+        user.setRoles(new HashSet<>());
+        Role role = roleService.getRoleByName("USER");
+        user.getRoles().add(role);
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
